5ff646fb5258698ee47b12046b17517baa3d3e52
Bowske <adabor6@st.amu.edu.pl>
2020-09-27 17:34:18 +0200

    gsap animation test workin - needed to change svgo config files so it does not get rid off id's created in figma

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <div id="app">
+  <div ref="gsapScena" id="app">
     <GsapScene />
   </div>
 </template>
 
 <script>
 import GsapScene from "../public/scena.svg";
+import gsap from "gsap";
 
 export default {
   name: "App",
   components: {
     GsapScene,
+  },
+  methods: {},
+  mounted: function () {
+    const [elements] = this.$refs.gsapScena.children; //destrukturyzacja tablicy(wrzucanie do niej childrenow refa)
+
+    console.log(elements);
+
+    const postac = elements.getElementById("postac");
+    const telefon = elements.getElementById("telefon");
+    const drzewa = elements.getElementById("drzewa");
+    const podloga = elements.getElementById("podloga");
+
+    gsap.set([telefon, postac, drzewa, podloga], { autoAlpha: 0 });
+
+    const tl = gsap.timeline({ defaults: { ease: "power3.inOut" } });
+
+    tl.fromTo(drzewa, { x: "0" }, { duration: 1, x: "0", autoAlpha: 1 })
+      .fromTo(
+        telefon,
+        { y: "-=300" },
+        { duration: 1, y: "+=300", autoAlpha: 1 }
+      )
+      .fromTo(postac, { x: "+=300" }, { duration: 1, x: "-=300", autoAlpha: 1 })
+      .fromTo(podloga, { x: "0" }, { duration: 1, x: "0", autoAlpha: 1 });
   },
 };
 </script>
@@ -27,7 +52,7 @@ export default {
   font-family: Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  background-color: #4d5664;
   display: flex;
   justify-content: center;
   align-items: center;
